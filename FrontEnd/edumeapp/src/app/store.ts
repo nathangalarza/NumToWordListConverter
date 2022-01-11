d20cd03623f9bfc82d43bea1bdb026490bb1d674
DESKTOP-TER4Q7J\Nate <nathanjavier0917@gmail.com>
2022-01-11 20:05:46 +0000

    Adding a slice setup for the numeric string value.

--- FrontEnd/edumeapp/src/app/store.ts
+++ FrontEnd/edumeapp/src/app/store.ts
@@ -1,9 +1,14 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import numberReducer from '../features/convert/numberSlice';
+
+//This is where the Store for the reducers are created
+
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    number: numberReducer
   },
 });
 
