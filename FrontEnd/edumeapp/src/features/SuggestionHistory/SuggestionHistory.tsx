bfa6a967ef800b8529bafc92b1cac2534b91499b
DESKTOP-TER4Q7J\Nate <nathanjavier0917@gmail.com>
2022-01-15 13:50:20 +0000

    Adding CSS changes to suggestions screen.

--- FrontEnd/edumeapp/src/features/SuggestionHistory/SuggestionHistory.tsx
+++ FrontEnd/edumeapp/src/features/SuggestionHistory/SuggestionHistory.tsx
@@ -4,12 +4,32 @@ import styles from "./SuggestionHistory.module.scss";
 
 export function SuggestionHistory() {
   const suggestions = useAppSelector(selectSuggestions);
+  let index = 0;
+  const readMore = (item : string) =>{
+console.log(item);
+  }
   const renderedListItems = suggestions.map((suggestId) => {
-    return <li> {suggestId + ","}</li>;
+    index++;
+    let item =  index + "-" + suggestId + "," 
+    + "  ";
+    debugger;
+    if(item.length > 50){
+    
+      return <li onClick={() => readMore(item)}> {item.substring(0, 50) + "... "}</li>
+      ;
+    }
+    else{
+
+      return <li> {item}</li>;
+    }
   });
   return (
     <div className={styles.history}>
+      <h2>Suggestions</h2>
       <li> {renderedListItems}</li>
+    
     </div>
   );
 }
+
+
